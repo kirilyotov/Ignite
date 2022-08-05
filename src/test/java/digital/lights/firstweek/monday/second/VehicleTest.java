af3b11a634a425736f08f9db0a70a3eb04a7b179
kyotov1 <kirilyotov@outlook.com>
2022-08-06 02:26:18 +0300

    Skeleton added

---
+++ src/test/java/digital/lights/firstweek/monday/second/VehicleTest.java
@@ -0,0 +1,20 @@
+package digital.lights.firstweek.monday.second;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class VehicleTest {
+
+    @Test
+    void getRegNumber() {
+    }
+
+    @Test
+    void fuelConsumption() {
+    }
+
+    @Test
+    void testHashCode() {
+    }
+}
