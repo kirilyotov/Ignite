46886adc16960f59545eaf90da437813d0da7055
kyotov1 <kirilyotov@outlook.com>
2022-08-05 19:50:39 +0300

    Car inherits Vehicle and overrides fuelConsumption()

---
+++ src/main/java/digital/lights/firstweek/monday/second/Car.java
@@ -0,0 +1,37 @@
+package digital.lights.firstweek.monday.second;
+
+import java.util.Objects;
+
+public class Car extends Vehicle {
+
+    static final byte CONSUMPTION_CONST = 3;
+    private final double engineVolume;
+
+    public Car(String regNumber, double engineVolume) {
+        super(regNumber);
+        if (!isCorrectEngineVolume(engineVolume)) {
+            throw new IllegalArgumentException("Not correct engine volume!");
+        }
+        this.engineVolume = engineVolume;
+    }
+
+    public double getEngineVolume() {
+        return engineVolume;
+    }
+
+    @Override
+    public double fuelConsumption() {
+        return getEngineVolume() * CONSUMPTION_CONST;
+    }
+
+    @Override
+    public String toString() {
+        return "Car with registration number: " + getRegNumber() + "has got " + getEngineVolume() + " liters engine " +
+                "volume and " + fuelConsumption() + "l/100 km consumption.";
+    }
+
+
+    private boolean isCorrectEngineVolume(double engineVolume) {
+        return engineVolume > 0;
+    }
+}
