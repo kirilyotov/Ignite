4624b076da429060c432a12f37a360631e1f9dfa
kyotov1 <kirilyotov@outlook.com>
2022-08-06 17:16:48 +0300

    Merge sort algorithm

--- src/main/java/digital/lights/firstweek/thursday/Merge.java
+++ src/main/java/digital/lights/firstweek/thursday/Merge.java
@@ -1,8 +1,68 @@
 package digital.lights.firstweek.thursday;
 
-public class Merge implements  SortAlgorithm{
+public class Merge implements SortAlgorithm {
     @Override
     public void sort(int[] arr) {
+        sort(arr, 0, arr.length - 1);
+    }
 
+    void merge(int[] arr, int l, int m, int r) {
+
+        int n1 = m - l + 1;
+        int n2 = r - m;
+
+
+        int[] L = new int[n1];
+        int[] R = new int[n2];
+
+
+        System.arraycopy(arr, l, L, 0, n1);
+        for (int j = 0; j < n2; ++j)
+            R[j] = arr[m + 1 + j];
+
+
+        int i = 0, j = 0;
+
+
+        int k = l;
+        while (i < n1 && j < n2) {
+            if (L[i] <= R[j]) {
+                arr[k] = L[i];
+                i++;
+            } else {
+                arr[k] = R[j];
+                j++;
+            }
+            k++;
+        }
+
+
+        while (i < n1) {
+            arr[k] = L[i];
+            i++;
+            k++;
+        }
+
+        /* Copy remaining elements of R[] if any */
+        while (j < n2) {
+            arr[k] = R[j];
+            j++;
+            k++;
+        }
+    }
+
+
+    void sort(int[] arr, int l, int r) {
+        if (l < r) {
+            // Find the middle point
+            int m = l + (r - l) / 2;
+
+            // Sort first and second halves
+            sort(arr, l, m);
+            sort(arr, m + 1, r);
+
+            // Merge the sorted halves
+            merge(arr, l, m, r);
+        }
     }
 }
