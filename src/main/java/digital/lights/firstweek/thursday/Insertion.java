ee6c6cf638750e584112a71094770ff20e8245ee
kyotov1 <kirilyotov@outlook.com>
2022-08-06 17:15:08 +0300

    Insertion sort algorithm

--- src/main/java/digital/lights/firstweek/thursday/Insertion.java
+++ src/main/java/digital/lights/firstweek/thursday/Insertion.java
@@ -1,8 +1,18 @@
 package digital.lights.firstweek.thursday;
 
-public class Insertion implements SortAlgorithm{
+public class Insertion implements SortAlgorithm {
     @Override
-    public void sort(int[] arr){
+    public void sort(int[] arr) {
 
+        for (int i = 1; i < arr.length; i++) {
+            int elem = arr[i];
+            int index = i - 1;
+
+            while (index >= 0 && arr[index] > elem) {
+                arr[index + 1] = arr[index];
+                index--;
+            }
+            arr[index + 1] = elem;
+        }
     }
 }
