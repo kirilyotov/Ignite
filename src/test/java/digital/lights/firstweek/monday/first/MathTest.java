d09252546882458b472c7274e3f1bdd092703d4d
kyotov1 <kirilyotov@outlook.com>
2022-08-06 15:53:53 +0300

    Deleted not used imports

--- src/test/java/digital/lights/firstweek/monday/first/MathTest.java
+++ src/test/java/digital/lights/firstweek/monday/first/MathTest.java
@@ -1,11 +1,9 @@
 package digital.lights.firstweek.monday.first;
 
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.CsvFileSource;
 import org.junit.jupiter.params.provider.CsvSource;
-import org.junit.jupiter.params.provider.ValueSource;
+
 
 import static org.junit.jupiter.api.Assertions.*;
 
