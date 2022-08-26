4a50c877f6106ca11ee4c3ced82281d2cf414719
kyotov1 <kirilyotov@outlook.com>
2022-08-26 23:28:49 +0300

    Figures

---
+++ src/main/java/digital/lights/secondweek/first/figures/Circle.java
@@ -0,0 +1,31 @@
+package digital.lights.secondweek.first.figures;
+
+import lombok.Getter;
+
+public class Circle extends Figure {
+
+    private final @Getter double  radius;
+
+
+    public Circle(double radius) {
+        if (radius <= 0)
+            throw new IllegalArgumentException("Circle radius should be positive number!");
+        this.radius = radius;
+    }
+
+    @Override
+    public double perimeter() {
+        return 2 * Math.PI * radius;
+    }
+
+    @Override
+    public double area() { return  Math.PI * radius * radius;}
+
+    @Override
+    public Figure clone(){return  new Circle(radius);}
+
+    @Override
+    public String toString() {
+        return String.format("circle %.2f", radius);
+    }
+}
---
+++ src/main/java/digital/lights/secondweek/first/figures/Figure.java
@@ -0,0 +1,8 @@
+package digital.lights.secondweek.first.figures;
+
+public abstract class Figure {
+    abstract double perimeter();
+
+    abstract double area();
+    public abstract Figure clone();
+}
