bd419c366fa5e3a62504755eaaeffa32545939c9
kyotov1 <kirilyotov@outlook.com>
2022-08-30 16:49:32 +0300

    Refactored Engine

--- Figure/src/main/java/lights/digital/commandline/CommandsInstance.java
+++ Figure/src/main/java/lights/digital/commandline/CommandsInstance.java
@@ -1,13 +1,13 @@
 package lights.digital.commandline;
 
-import lights.digital.createfigures.CreateFigures;
+import lights.digital.engine.Engine;
 
 /**
  * Interface class with function to be implemented in enum class.
  */
 public interface CommandsInstance {
 
-    CreateFigures createFigures = new CreateFigures();
+    Engine createFigures = new Engine();
 
     void getCommand() throws Exception;
 
