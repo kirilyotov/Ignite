e36b44160c5ba84db8246d2170e1a6ffeefb104b
kyotov1 <kirilyotov@outlook.com>
2022-08-30 16:46:59 +0300

    Instructions added

--- Figure/src/main/java/lights/digital/utils/Utils.java
+++ Figure/src/main/java/lights/digital/utils/Utils.java
@@ -3,6 +3,17 @@ package lights.digital.utils;
 public class Utils {
     public static void help() {
         System.out.println("""
+                Instructions:
+                ---------------------------------------------
+                random          generate random figure
+                stdin           read figures form console
+                file            read figures form file
+                delete          delete element on position
+                duplicate       duplicate element on position
+                save            save all figures to file
+                help            instructions 
+                exit            exit form program
+                ----------------------------------------------
                 """);
     }
 }
