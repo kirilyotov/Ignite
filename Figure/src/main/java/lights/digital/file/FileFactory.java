988c8ff089a867ddaae03d044df0dea0899dd65a
kyotov1 <kirilyotov@outlook.com>
2022-09-07 02:51:45 +0300

    Interface method parameters changed and corrected exceptions

--- Figure/src/main/java/lights/digital/file/FileFactory.java
+++ Figure/src/main/java/lights/digital/file/FileFactory.java
@@ -5,6 +5,7 @@ import lights.digital.figures.Figure;
 import java.util.Vector;
 
 public interface FileFactory {
-    Vector<String > readAllFile();
-    void saveToFile(Vector<Figure> figures);
+    Vector<String> readAllFile(String path);
+
+    void saveToFile(String path, Vector<Figure> figures);
 }
--- Figure/src/main/java/lights/digital/file/FileIO.java
+++ Figure/src/main/java/lights/digital/file/FileIO.java
@@ -7,19 +7,17 @@ import java.util.Vector;
 
 public class FileIO implements FileFactory {
 
-    private String path;
-
-    public FileIO(String path) {
-        this.path = path;
-    }
 
     /**
      * Reads all data of file and saving it to vector.
+     *
      * @return vector with figures.
      */
-    public Vector<String > readAllFile() {
-
+    public Vector<String> readAllFile(String path) {
+        if (path == null)
+            throw new NullPointerException("Not correct path!");
         Vector<String> data = new Vector<>();
+
         try (BufferedReader bufferedReader = new BufferedReader(new FileReader(path))) {
             String line = bufferedReader.readLine();
 
@@ -29,9 +27,9 @@ public class FileIO implements FileFactory {
             }
 
         } catch (FileNotFoundException e) {
-            throw new RuntimeException(e);
+            throw new IllegalArgumentException("Not correct path!");
         } catch (IOException e) {
-            throw new RuntimeException(e);
+            throw new RuntimeException("Problem with reading file");
         }
 
         return data;
@@ -39,9 +37,16 @@ public class FileIO implements FileFactory {
 
     /**
      * Saving all data to file.
+     *
      * @param figures vector with figures..
      */
-    public void saveToFile(Vector<Figure> figures) {
+    public void saveToFile(String path, Vector<Figure> figures) {
+        if (path == null || figures == null) {
+            throw new NullPointerException("Null pointer given!");
+        }
+        if (figures.isEmpty())
+            throw new IllegalArgumentException("Vector is empty!");
+
         try (BufferedWriter bw = new BufferedWriter(new FileWriter(path))) {
             for (int i = 0; i < figures.size(); i++) {
                 if (i > 0)
