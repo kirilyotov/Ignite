d4bb96959ac6993bf5e45d6d72e3c82b0a7590c8
kyotov1 <kirilyotov@outlook.com>
2022-09-07 02:52:19 +0300

    new line added

--- Figure/src/main/java/lights/digital/utils/Utils.java
+++ Figure/src/main/java/lights/digital/utils/Utils.java
@@ -14,6 +14,7 @@ public class Utils {
                 help            instructions 
                 exit            exit form program
                 ----------------------------------------------
+                                
                 """);
     }
 }
