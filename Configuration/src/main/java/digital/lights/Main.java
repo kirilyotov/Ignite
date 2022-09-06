dc8e6986d38b1175ed00a1a7f014d60738b24ecc
kyotov1 <kirilyotov@outlook.com>
2022-09-07 02:49:03 +0300

    Update

--- Configuration/src/main/java/digital/lights/Main.java
+++ Configuration/src/main/java/digital/lights/Main.java
@@ -1,7 +1,11 @@
 package digital.lights;
 
+import java.util.Locale;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        String line = "jAva";
+        line = line.substring(0,1).toUpperCase() + line.substring(1).toLowerCase();
+        System.out.println(line);
     }
 }
