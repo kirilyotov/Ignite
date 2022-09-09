421ff06c642a3443a5301f94689f453b4208278d
kyotov1 <kirilyotov@outlook.com>
2022-09-09 03:10:30 +0300

    Create IniConfigurationReader.java

---
+++ Configuration/src/main/java/digital/lights/factory/IniConfigurationReader.java
@@ -0,0 +1,20 @@
+package digital.lights.factory;
+
+import digital.lights.configuration.Configuration;
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.configuration.ConfigurationException;
+import org.apache.commons.configuration.HierarchicalINIConfiguration;
+
+public class IniConfigurationReader implements ConfigurationReader {
+    @Override
+    public Configuration create(CommandLine cmd) {
+        HierarchicalINIConfiguration configuration;
+        try {
+            configuration = new HierarchicalINIConfiguration(cmd.getOptionValue("c"));
+        } catch (ConfigurationException e) {
+            throw new RuntimeException(e);
+        }
+        return new Configuration(configuration.getString("log.LogPath"), configuration.getString("log.LogLevel"));
+    }
+
+}
