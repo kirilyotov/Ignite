6a8a0a33a0e16b4bf129d801adccea4b2f7ed4ca
kyotov1 <kirilyotov@outlook.com>
2022-09-09 03:10:09 +0300

    Update

--- Configuration/src/main/java/digital/lights/Main.java
+++ Configuration/src/main/java/digital/lights/Main.java
@@ -1,11 +1,20 @@
 package digital.lights;
 
-import java.util.Locale;
+
+import digital.lights.parser.CmdParser;
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.ParseException;
 
 public class Main {
+
     public static void main(String[] args) {
-        String line = "jAva";
-        line = line.substring(0,1).toUpperCase() + line.substring(1).toLowerCase();
-        System.out.println(line);
+        try {
+            CommandLine cmd = CmdParser.parse(args);
+            CmdParser.getInstance(cmd);
+        } catch (ParseException e) {
+            System.out.println("Problem with parsing!");
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+        }
     }
 }
